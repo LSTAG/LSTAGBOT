d02b8dd86ee681c3de81d4cd6424903319e72c70
OBNinjaa <64389208+OBNinjaa@users.noreply.github.com>
2022-04-15 16:37:46 +0100

    Updating to discord.js version 13

---
+++ src/commands/misc/help.js
@@ -0,0 +1,88 @@
+/**
+ * @file Dynamic help command
+ * @author Naman Vrati
+ * @since 1.0.0
+ */
+
+const { prefix } = require("./../../config.json");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "help",
+  description: "List all commands of bot or info about a specific command.",
+  aliases: ["commands"],
+  usage: "[command name]",
+  cooldown: 5,
+
+  /**
+   * @description Executes when the command is called by command handler.
+   * @author Naman Vrati
+   * @param {Object} message The Message Object of the command.
+   * @param {String[]} args The Message Content of the received message seperated by spaces (' ') in an array, this excludes prefix and command/alias itself.
+   */
+
+  execute(message, args) {
+    const { commands } = message.client;
+
+    if (!args.length) {
+      /**
+       * @type {Object}
+       * @description Help command embed object
+       */
+
+      let helpEmbed = new MessageEmbed()
+        .setColor(0x4286f4)
+        .setURL(process.env.URL)
+        .setTitle("List of all my commands")
+        .setDescription("`" + commands.map((command) => command.name).join("`, `") + "`")
+
+        .addField(
+          "Usage",
+          `\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`
+        );
+
+      return message.channel.send({ embeds: [helpEmbed] }).catch((error) => {
+        console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+
+        message.reply({ content: "It seems like I can't DM you!" });
+      });
+    }
+
+    /**
+     * @type {String}
+     * @description First argument in lower case
+     */
+
+    const name = args[0].toLowerCase();
+
+    /**
+     * @type {Object}
+     * @description The command object
+     */
+
+    const command =
+      commands.get(name) || commands.find((c) => c.aliases && c.aliases.includes(name));
+
+    if (!command) {
+      return message.reply({ content: "That's not a valid command!" });
+    }
+
+    /**
+     * @type {Object}
+     * @description Embed of Help command for a specific command.
+     */
+
+    let commandEmbed = new MessageEmbed().setColor(0x4286f4).setTitle(`Command: ${command.name}`);
+
+    if (command.description) commandEmbed.setDescription(`${command.description}`);
+
+    if (command.aliases)
+      commandEmbed
+        .addField("Aliases", `\`${command.aliases.join(", ")}\``, true)
+        .addField("Cooldown", `${command.cooldown || 3} second(s)`, true);
+    if (command.usage)
+      commandEmbed.addField("Usage", `\`${prefix}${command.name} ${command.usage}\``, true);
+
+    message.channel.send({ embeds: [commandEmbed] });
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,81 +1,121 @@
-const {
-	AkairoClient,
-	CommandHandler,
-	ListenerHandler,
-	InhibitorHandler,
-} = require("discord-akairo");
 const mineflayer = require("mineflayer");
-
-const settings = require("./config/config.json");
-const { token, username, password, host, port, version, auth } = settings;
 const colors = require("colors");
-const { config } = require("dotenv");
-const { join } = require("path");
+const path = require("path");
+const fs = require("fs");
 
-config();
+const { Client, Collection, Intents } = require("discord.js");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
 
-if (!host || !username || !password || !version || !auth) {
-	console.log("[ERROR] Please fill out config.json".red);
-	process.exit(1);
+if (
+  Number(process.version.slice(1).split(".")[0]) < 10 &&
+  Number(process.version.slice(1).split(".")[0]) < 4
+) {
+  throw new Error("You must be using node version 10.4.0 or higher to use this bot.");
 }
 
-const bot = mineflayer.createBot({
-	host: host,
-	username: username,
-	password: password,
-	port: port || null,
-	version: version,
-	auth: auth,
-	verbose: true,
+const config = require("./config.json");
+const { host, port, auth, username, password, guild, token, prefix, owner, ownername } = config;
+if (
+  !host ||
+  !auth ||
+  !username ||
+  !password ||
+  !guild ||
+  !token ||
+  !prefix ||
+  !owner ||
+  !ownername
+) {
+  console.error(`[!] You must fill out the config!`.red);
+  console.log(`[!] Located in ${__dirname.yellow}`.red);
+  process.exit(1);
+}
+
+function ScriptsLoad(bot) {
+  const COMMANDS_DIR = path.join(__dirname, "plugins");
+  const EVENTS_DIR = path.join(__dirname, "events/mineflayer");
+
+  const commands = fs
+    .readdirSync(COMMANDS_DIR)
+    .filter((x) => x.endsWith(".js"))
+    .map((pluginName) => require(path.join(COMMANDS_DIR, pluginName)));
+  const events = fs
+    .readdirSync(EVENTS_DIR)
+    .filter((x) => x.endsWith(".js"))
+    .map((pluginName) => require(path.join(EVENTS_DIR, pluginName)));
+
+  bot.loadPlugins(commands);
+  bot.loadPlugins(events);
+}
+
+function initialize() {
+  let bot = mineflayer.createBot({
+    host: host,
+    port: port || 25565,
+    auth: auth,
+    username: username,
+    password: password,
+    hideErrors: true,
+  });
+  module.exports = bot;
+  ScriptsLoad(bot);
+
+  // When the bot is disconnected, reconnect it.
+  bot.on("end", (reason) => {
+    console.log(
+      `[${new Date().toLocaleTimeString().gray}] ${`Disconnected`.red}`,
+      colors.yellow(`Attemp to reconnect in 5 seconds`)
+    );
+    setTimeout(() => initialize(), 5000);
+  });
+}
+
+initialize();
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-module.exports = { bot };
+/**
+ * @description All event files of the event handler.
+ * @type {String[]}
+ */
 
-if (username === bot.username) return;
+const eventFiles = fs.readdirSync("./src/events/discord").filter((file) => file.endsWith(".js"));
 
-const commandsPath = join(__dirname, "..", "commands/");
-const listenersPath = join(__dirname, "..", "listeners/");
-
-class Akairo extends AkairoClient {
-	constructor() {
-		super(
-			{
-				ownerID: "709202831760162910",
-			},
-			{
-				disableEveryone: true,
-			}
-		);
-
-		this.commandHandler = new CommandHandler(this, {
-			prefix: ["c."],
-			blockBots: true,
-			blockClient: true,
-			allowMention: true,
-			defaultCooldown: 800,
-			commandUtil: true,
-			directory: join(__dirname, "commands"),
-		});
-
-		this.listenerHandler = new ListenerHandler(this, {
-			directory: join(__dirname, "listeners"),
-		});
-		this.inhibitorHandler = new InhibitorHandler(this, {
-			directory: join(__dirname, "./inhibitors/"),
-		});
-
-		this.listenerHandler.setEmitters({
-			commandHandler: this.commandHandler,
-			inhibitorHandler: this.inhibitorHandler,
-			listenerHandler: this.listenerHandler,
-		});
-
-		this.inhibitorHandler.loadAll();
-		this.listenerHandler.loadAll();
-		this.commandHandler.loadAll();
-	}
+for (const file of eventFiles) {
+  const event = require(`./events/discord/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, async (...args) => await event.execute(...args, client));
+  }
 }
 
-const client = new Akairo();
+client.commands = new Collection();
+client.slashCommands = new Collection();
+client.buttonCommands = new Collection();
+client.selectCommands = new Collection();
+client.contextCommands = new Collection();
+client.cooldowns = new Collection();
+client.triggers = new Collection();
+
+/**
+ * @type {String[]}
+ * @description All command categories aka folders.
+ */
+
+const commandFolders = fs.readdirSync("./src/commands");
+
+for (const folder of commandFolders) {
+  const commandFiles = fs
+    .readdirSync(`./src/commands/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const command = require(`./commands/${folder}/${file}`);
+    client.commands.set(command.name, command);
+  }
+}
 
 client.login(token);
