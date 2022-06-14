22d439acd79b517e4c5184f3e0720f3282c1bd8c
OBNinjaa <64389208+OBNinjaa@users.noreply.github.com>
2022-06-14 21:27:38 +0100

    Delete mine.js

--- src/plugins/mine.js
+++
@@ -1,9 +0,0 @@
-const collectBlock = require("mineflayer-collectblock").plugin;
-const mineflayer = require("mineflayer");
-const colors = require("colors");
-
-/**
- * @param {mineflayer.Bot} bot
- */
-
-module.exports = (bot) => {};
